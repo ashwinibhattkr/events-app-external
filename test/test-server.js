0148d8d3bb001c8e8d3a9cccac2a3f9fb5ad6346
ransahu1 <99858253+ransahu1@users.noreply.github.com>
2022-02-18 16:13:58 +0530

    changing testcases

--- test/test-server.js
+++ test/test-server.js
@@ -25,7 +25,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Team2 - Cloud Event Manager's application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1  style="text-align: center;">Welcome to Team2 - Cloud Event Manager's application</h1>"));
         return done();
       });
 
